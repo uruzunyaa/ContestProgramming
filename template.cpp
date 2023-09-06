058f8fa042f79ef6ec8911a6050616193a909c84
uruzunyaa <110530704+uruzunyaa@users.noreply.github.com>
2023-09-06 13:46:14 +0900

    テンプレ、loopの変更

--- template.cpp
+++ template.cpp
@@ -4,8 +4,8 @@
 #include <deque>
 using namespace std;
 
-//loop(ループ変数、初期値、ループ回数)
-#define loop(i,m,n) for(int i=m;i<n+m;i++)
+//loop(ループ変数、初期値、最終ループ値)
+#define loop(i,m,n) for(int i=m;i<=n;i++)
 
 //rep(ループ変数、ループ数)
 #define rep(i,n) for(int i=0;i<n;i++)
