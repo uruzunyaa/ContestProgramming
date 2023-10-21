0a56964fd7bc3a24d6208539f4a057c075ca1fb1
uruzunyaa <110530704+uruzunyaa@users.noreply.github.com>
2023-10-21 10:49:59 +0900

    c

---
+++ AfterCpp23Copy/ProgrammingQuiz/hard/one/main.c
@@ -0,0 +1,6 @@
+//上級問題1,C言語
+#include <stdio.h>
+int main(){
+	printf("%d\n",-5%4);
+	return 0;
+}
---
+++ AfterCpp23Copy/ProgrammingQuiz/hard/one/main.cpp
@@ -0,0 +1,7 @@
+//上級問題1,C++
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<5%4<<endl;
+	return 0;
+}
